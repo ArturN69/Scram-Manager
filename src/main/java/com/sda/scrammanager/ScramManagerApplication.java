f073dc431f39517757abceb488fe5a554269e7ee
Artur Noworyta <arturno69@gmail.com>
2022-02-20 11:22:06 +0100

    first commit

---
+++ src/main/java/com/sda/scrammanager/ScramManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sda.scrammanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScramManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScramManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/scrammanager/ScramManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.scrammanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScramManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
